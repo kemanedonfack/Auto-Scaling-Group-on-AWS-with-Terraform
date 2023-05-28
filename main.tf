f3beb5363ac3933f10c1380185d3c39471b6bc81
Kemane Donfack <kemanedonfack5@gmail.com>
2023-05-29 00:23:38 +0100

    add launch template and asg policies

--- main.tf
+++ main.tf
@@ -1,29 +1,47 @@
-resource "aws_launch_configuration" "instances_configuration" {
-  name_prefix     = "asg-instance"
-  image_id        = var.ami
-  key_name        = var.key_name
-  instance_type   = var.instance_type
-  user_data       = file("install_script.sh")
-  security_groups = [aws_security_group.instance_sg.id]
+resource "aws_launch_template" "instances_configuration" {
+  name_prefix            = "asg-instance"
+  image_id               = var.ami
+  key_name               = var.key_name
+  instance_type          = var.instance_type
+  user_data              = filebase64("install_script.sh")
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
   lifecycle {
     create_before_destroy = true
   }
+
+  tags = {
+    Name = "asg-instance"
+  }
+  
 }
 
 resource "aws_autoscaling_group" "asg" {
-  name                 = "asg"
-  min_size             = 2
-  max_size             = 4
-  desired_capacity     = 2
-  launch_configuration = aws_launch_configuration.instances_configuration.name
-  vpc_zone_identifier  = [aws_subnet.first_public_subnet.id, aws_subnet.second_public_subnet.id]
-
-  tag {
-    key                 = "Name"
-    value               = "ASG Instance"
-    propagate_at_launch = true
+  name                      = "asg"
+  min_size                  = 2
+  max_size                  = 4
+  desired_capacity          = 2
+  health_check_grace_period = 150
+  health_check_type         = "ELB"
+  vpc_zone_identifier       = [aws_subnet.first_public_subnet.id, aws_subnet.second_public_subnet.id]
+  launch_template {
+    id      = aws_launch_template.instances_configuration.id
+    version = "$Latest"
   }
+}
+
+resource "aws_autoscaling_policy" "avg_cpu_policy_greater" {
+  name                   = "avg-cpu-policy-greater"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.asg.id
+  # CPU Utilization is above 50
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0
+  }
+
 }
 
 resource "aws_lb" "alb" {
