34befb4babcb8a8516e3f0fa5f0b9c127d9bd989
Kemane Donfack <kemanedonfack5@gmail.com>
2023-05-29 02:09:19 +0100

    remove loadbalancer code

--- main.tf
+++ main.tf
@@ -49,32 +49,6 @@ resource "aws_autoscaling_policy" "avg_cpu_policy_greater" {
 
 }
 
-resource "aws_lb" "alb" {
-  name               = "asg-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.first_public_subnet.id, aws_subnet.second_public_subnet.id]
-}
-
-resource "aws_lb_listener" "alb_listener" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_target_group.arn
-  }
-}
-
-resource "aws_lb_target_group" "alb_target_group" {
-  name     = "asg-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.infrastructure_vpc.id
-}
-
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.asg.id
   lb_target_group_arn    = aws_lb_target_group.alb_target_group.arn
